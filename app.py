2b83c9b89198bb67b2e5bdf6460b0df85efad665
krunaljogani <38317404+krunaljogani@users.noreply.github.com>
2025-07-31 14:52:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,8 @@ def get_holdings(user_session):
                 "Total Qty": float(item.get("netQty")),
                 "Current Price": float(item.get("symbol", [{}])[0].get("ltp")),
                 "Buy Price": float(item.get("buyPrice")),
-                "Buy Value": BuyValue=float(item.get("buyPrice"))*float(item.get("netQty")),
-                "Current Value": CurrentValue=float(item.get("symbol", [{}])[0].get("ltp"))*float(item.get("netQty")),
+                "Buy Value": BuyValue = float(item.get("buyPrice"))*float(item.get("netQty")),
+                "Current Value": CurrentValue = float(item.get("symbol", [{}])[0].get("ltp"))*float(item.get("netQty")),
                 "Day PnL": 0,
                 "Total PnL": CurrentValue - BuyValue,
             })
